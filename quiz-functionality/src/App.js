5fa1934877d87fcb9dfa433f7a0816245bf7a1b6
chris burzo <gm.c.burzo@gmail.com>
2023-02-09 10:32:16 -0500

    i

--- quiz-functionality/src/App.js
+++ quiz-functionality/src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [questionObject, setQuestionObject] = useState({});
   const [answersArray, setAnswersArray] = useState([]);
   const [toNextQuestion, seToNextQuestion] = useState(false);
+  const [challengeBucket, setChallengeBucket] = useState('');
 
   useEffect(()=>{
     let qObj = {
@@ -39,47 +40,72 @@ function App() {
     let qArr = questionObject.questionArray;
     let aArr = answersArray;
 
-    qArr[i].selectStatus = true;
+    if(qObj.questionItself < 5){
+      qArr[i].selectStatus = true;
 
-    let x = 0;
+      let x = 0;
 
-    while(x < qArr.length){
-      if(x !== i){
-        qArr[x].selectStatus = false;
+      while(x < qArr.length){
+        if(x !== i){
+          qArr[x].selectStatus = false;
+        }
+
+        x++;
       }
 
-      x++;
+      qObj.questionArray = qArr;
+
+      let aObj = {
+        questionItself: qObj.questionItself,
+        answer: i + 1
+      };
+
+      aArr[qObj.questionItself - 1] = aObj;
+
+      setAnswersArray([...aArr]);
+      setQuestionObject({...qObj});
+      seToNextQuestion(true);
     }
-
-    qObj.questionArray = qArr;
-
-    let aObj = {
-      questionItself: qObj.questionItself,
-      answer: i + 1
-    };
-
-    aArr[qObj.questionItself - 1] = aObj;
-
-    setAnswersArray([...aArr]);
-    setQuestionObject({...qObj});
-    seToNextQuestion(true);
   } 
 
   const nextHandler = () => {
     let qObj = questionObject;
     let qArr = questionObject.questionArray;
+    let aArr = answersArray;
 
-    qObj.questionItself++;
+    if(qObj.questionItself === aArr.length){
+      qObj.questionItself++;
+      let x = 0;
 
-    let x = 0;
+      while(x < qArr.length){
+        qArr[x].selectStatus = false;
+        x++;
+      }
 
-    while(x < qArr.length){
-      qArr[x].selectStatus = false;
-      x++;
+      setQuestionObject({...qObj});
+      seToNextQuestion(false);
+    }else{
+      qObj.questionItself++;
+
+      let gotNum = aArr[qObj.questionItself - 1].answer - 1;
+
+      qArr[gotNum].selectStatus = true;
+
+      let x = 0;
+
+      while(x < qArr.length){
+        if(x !== gotNum){
+          qArr[x].selectStatus = false;
+        }
+
+        x++;
+      }
+
+      qObj.questionArray = qArr;
+
+      setQuestionObject({...qObj});
+      seToNextQuestion(true);
     }
-
-    setQuestionObject({...qObj});
-    seToNextQuestion(false);
   }
 
   const backHandler = () => {
@@ -87,31 +113,73 @@ function App() {
     let qArr = questionObject.questionArray;
     let aArr = answersArray;
 
-    qObj.questionItself--;
+    if(qObj.questionItself > 1){
+      qObj.questionItself--;
 
-    console.log(qObj.questionItself - 1,aArr[qObj.questionItself - 1].answer);
+      let gotNum = aArr[qObj.questionItself - 1].answer - 1;
 
-    let gotNum = aArr[qObj.questionItself - 1].answer - 1;
+      qArr[gotNum].selectStatus = true;
 
-    qArr[gotNum].selectStatus = true;
+      let x = 0;
 
-    let x = 0;
+      while(x < qArr.length){
+        if(x !== gotNum){
+          qArr[x].selectStatus = false;
+        }
 
-    while(x < qArr.length){
-      if(x !== gotNum){
-        qArr[x].selectStatus = false;
+        x++;
       }
 
-      x++;
+      qObj.questionArray = qArr;
+
+      setQuestionObject({...qObj});
+      seToNextQuestion(true);
     }
-
-    qObj.questionArray = qArr;
-
-    setQuestionObject({...qObj});
-    seToNextQuestion(true);
   }
 
-  console.log(answersArray);
+  const calculateChallengeBucket = () =>{
+    let aArr = answersArray;
+
+    //console.log(aArr);
+
+    let getOne = aArr.filter(i =>{
+      return i.answer === 1;
+    });
+
+    let getTwo = aArr.filter(i =>{
+      return i.answer === 2;
+    });
+
+    let getThree = aArr.filter(i =>{
+      return i.answer === 3;
+    });
+
+    let getFour = aArr.filter(i =>{
+      return i.answer === 4;
+    });
+
+    let concatentatedValues = [
+      getOne.length, 
+      getTwo.length,
+      getThree.length,
+      getFour.length
+    ];
+
+    let cb = concatentatedValues.indexOf(Math.max(...concatentatedValues)) + 1;
+  
+    if(cb === 1){
+      setChallengeBucket('challengeBucket 1');
+    }else if(cb === 2){
+      setChallengeBucket('challengeBucket 2');
+    }else if(cb === 3){
+      setChallengeBucket('challengeBucket 3');
+    }
+    else{
+      setChallengeBucket('challengeBucket 4');
+    }
+  }
+
+  console.log(challengeBucket);
 
   return (
     <div className="App">
@@ -119,7 +187,7 @@ function App() {
       questions per week</p>
 
       {
-        questionObject.questionArray?questionObject.questionArray.map((i, index) => {
+        questionObject.questionArray && questionObject.questionItself < 5?questionObject.questionArray.map((i, index) => {
           return (
             <div key ={Math.random()}>
               <button onClick={()=>selectAnswerHandler(index)}
@@ -130,9 +198,17 @@ function App() {
           )
         }):null
       }
+      {
+        questionObject.questionItself === 5?answersArray.map(i =>{
+          return (<div key={Math.random()}>
+            <p>question{i.questionItself}: {i.answer}</p>
+          </div>);
+        }):null
+      }
       <button onClick={backHandler}>Back</button>
-      <button onClick={nextHandler} 
-      disabled={toNextQuestion?false: true}>Next</button>
+      {questionObject.questionItself < 5?<button onClick={nextHandler} 
+      disabled={toNextQuestion?false: true}>Next</button>: 
+      <button onClick={calculateChallengeBucket}>Submit</button>}
     </div>
   );
 }
